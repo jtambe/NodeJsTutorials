4d04e70ab624177960231ce8516ff0ebd1e67083
Jayesh Tambe <tambe_jayesh@hotmail.com>
2017-04-08 14:56:29 -0700

    cookie and session

--- cookiesession/server.js
+++ cookiesession/server.js
@@ -1,21 +1,35 @@
 
 // npm install express --save
 // npm install morgan --save
+// npm install cookie-parser --save
+// npm install express-session --save
 
 var express = require("express");
 var app = express();
-
-
 var port = process.env.PORT || 8080;
+
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
 
 // all traffic passes through morgan get, post etc
 var morgan = require("morgan");
 app.use(morgan("dev"));
+// all traffic passes through cookieparser
+app.use(cookieParser());
+// all traffic passes through session
+app.use(session({
+				secret: "anystringtext",
+				saveUninitialized: true,
+				resave: true
+				}));
 
 
 app.use("/", function(req,res){
 	// express function
 	res.send("this is being sent via express res.send");
+	console.log(req.cookies);
+	console.log("*******");
+	console.log(req.session);
 });
 
 
